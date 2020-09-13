0ba2bafd437317e8453566623b7049ba26d593f3
twodayslate <zac@gorak.us>
2020-09-13 16:32:04 -0400

    Initial Commit

---
+++ nested-navs/nested_navsApp.swift
@@ -0,0 +1,17 @@
+//
+//  nested_navsApp.swift
+//  nested-navs
+//
+//  Created by Zachary Gorak on 9/13/20.
+//
+
+import SwiftUI
+
+@main
+struct nested_navsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
